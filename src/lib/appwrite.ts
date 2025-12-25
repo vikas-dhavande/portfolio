f650bf99ee4d02f9066603b55b1bb534ebe58e62
vikas-dhavande <235853210+vikas-dhavande@users.noreply.github.com>
2025-12-25 23:02:45 +0530

    feat: Add Appwrite client and service initialization.

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,12 @@
+import { Client, Account, Databases, Storage } from 'appwrite';
+
+export const client = new Client();
+
+client
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export { ID } from 'appwrite';
